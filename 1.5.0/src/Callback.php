ffc26f8103f108275522118927da1e2a123425ac
Michael Otieno <mikeotizels@gmail.com>
2025-03-02 13:03:28 +0300

    Update Callback.php

--- 1.5.0/src/Callback.php
+++ 1.5.0/src/Callback.php
@@ -2,9 +2,8 @@
 /**
  * M-PESA Callback API
  * 
- * @package    Mikeotizels/APIs/ThirdParty/Safaricom/Mpesa
  * @author     Michael Otieno <mikeotizels@gmail.com>
- * @copyright  Copyright 2020-2022 Michael Otieno
+ * @copyright  Copyright 2022-2023 Michael Otieno
  * @license    Licensed under The MIT License (MIT). For the license terms, 
  *             please see the LICENSE file that was distributed with this 
  *             source code or visit <https://opensource.org/licenses/MIT>.
