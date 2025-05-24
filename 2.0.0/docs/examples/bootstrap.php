fd757917642ea8885d87efba27f3a04c0011146a
Michael Otieno <mikeotizels@gmail.com>
2025-05-25 00:26:51 +0300

    Delete 2.0.0 directory

--- 2.0.0/docs/examples/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Load the SDK autoload.php file.
-require_once dirname(__DIR__) . '/autoload.php';
-
-// Instantiate the Mpesa object.
-$mpesa = new Mikeotizels\Mpesa\Mpesa([
-	'environment' => 'testing'
-]);
